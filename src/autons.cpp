92b232749aea27b9fdeabcb63f53412ecfe9d050
SantaBoy23 <bbsanti969@gmail.com>
2025-01-23 17:30:46 -0500

    shits ass

--- src/autons.cpp
+++ src/autons.cpp
@@ -73,12 +73,12 @@ chassis.pid_wait(); //wait for drive to finish
   chassis.pid_wait_quick_chain(); //wait for drive to finish
  chassis.pid_turn_set(37.5_deg, DRIVE_SPEED);
    chassis.pid_wait_quick_chain(); //wait for drive to finish
-   chassis.pid_drive_set(-33_in, DRIVE_SPEED, true);
+   chassis.pid_drive_set(-30_in, DRIVE_SPEED, true);
      chassis.pid_wait_quick_chain(); //wait for drive to finish
      chassis.pid_turn_set(90_deg, DRIVE_SPEED);
    chassis.pid_wait_quick_chain(); //wait for drive to finish
 
-    chassis.pid_drive_set(-9.5_in, DRIVE_SPEED, true);
+    chassis.pid_drive_set(-9.5_in, DRIVE_SPEED, true); //change made here
        chassis.pid_wait_quick_chain(); //wait for drive to finish
            chassis.pid_drive_set(2_in, DRIVE_SPEED, true);
        chassis.pid_wait(); //wait for drive to finish
@@ -86,20 +86,19 @@ chassis.pid_wait(); //wait for drive to finish
        intake.move(127); //start spinning intake
        chassis.pid_drive_set(3.5_in, DRIVE_SPEED, true);
        chassis.pid_wait_quick_chain(); //wait for drive to finish
-                 pros::delay (250);               
+                 pros::delay(250);               
       chassis.pid_turn_set(181_deg, DRIVE_SPEED);
    chassis.pid_wait_quick_chain(); //wait for drive to finish
           chassis.pid_drive_set(-50_in, 82, true);
              chassis.pid_wait(); //wait for drive to finish
                     pros::delay (250); //start spinning intake
-                       chassis.pid_drive_set(-10_in, 82, true);
+  chassis.pid_drive_set(-12_in, 82, true); //first ring drive ahdshfsafjjsa
              chassis.pid_wait(); //wait for drive to finish
-
            chassis.pid_drive_set(8_in, DRIVE_SPEED, true);
              chassis.pid_wait();
-                                   chassis.pid_turn_set(70_deg, DRIVE_SPEED);
-                            chassis.pid_wait_quick_chain();
-       chassis.pid_drive_set(-8_in, DRIVE_SPEED, true);
+  chassis.pid_turn_set(70_deg, DRIVE_SPEED);
+              chassis.pid_wait_quick_chain();
+       chassis.pid_drive_set(-10_in, DRIVE_SPEED, true);
         chassis.pid_wait_quick_chain();
          chassis.pid_drive_set(5_in, DRIVE_SPEED, true);
                   chassis.pid_wait_quick_chain();
